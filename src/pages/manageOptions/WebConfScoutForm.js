89826b2c29e8d3ca830a7cf291c904c44c8e4392
Ban Hun <banhun@Banui-MacBookPro.local>
2024-10-08 10:32:22 +0900

    web_conf_scout 설명 추가

--- src/pages/manageOptions/WebConfScoutForm.js
+++ src/pages/manageOptions/WebConfScoutForm.js
@@ -14,7 +14,17 @@ const WebConfScoutForm = ({ handleOptionChange }) => {
 	return (
 		<>
 			<h4 className={`${textColorClass}`}>Options</h4>
-			<Accordion data-bs-theme={`${theme}`}>
+			<Accordion defaultActiveKey="0" data-bs-theme={`${theme}`}>
+				<Accordion.Item eventKey="0">
+					<Accordion.Header>❗️ 계정에 Root 권한이 없는 경우.</Accordion.Header>
+					<Accordion.Body>
+						1. 서버에 root로 로그인합니다.<br />
+						2. 아래 명령어를 사용하여 스크립트를 다운 받습니다.<br />
+						<strong>wget</strong> https://raw.githubusercontent.com/iteasy-ops-dev/ansible.roles.web_conf_scout/refs/heads/main/file/web_conf_scout.sh<br />
+						3. 아래 명령어를 사용하여 정보를 얻습니다.<br />
+						4. (가능하면)해당 쉘 스크립트를 삭제합니다.<br />
+					</Accordion.Body>
+				</Accordion.Item>
 				<Accordion.Item>
 					<Accordion.Header>Info</Accordion.Header>
 					<Accordion.Body>
